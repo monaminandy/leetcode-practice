e051495e158b2070426cf372d3ad2fa22e60a026
Monami Nandy <142199074+monaminandy@users.noreply.github.com>
2025-02-03 09:51:40 +0530

    Create pascal_triangle.py

---
+++ easy/pascal_triangle.py
@@ -0,0 +1,19 @@
+#Pascal Triangle
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        triangle = []
+
+        for i in range(numRows):
+            row = []
+            for j in range(i + 1):
+                if j == 0 or j == i:  # First and last elements are always 1
+                    row.append(1)
+                else:
+                    row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            triangle.append(row)
+        return triangle
