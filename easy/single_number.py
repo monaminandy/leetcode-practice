01468ac158c200a362d32dc3bd3b80db99df9722
Monami Nandy <142199074+monaminandy@users.noreply.github.com>
2025-02-03 08:53:55 +0530

    Create single_number.py

---
+++ easy/single_number.py
@@ -0,0 +1,13 @@
+#Single Number
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total=0
+        for i in nums:
+            total ^= i
+        return total
+        
